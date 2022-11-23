27e696dff833362b25c6a375495c6b233a7f56b6
wlad184 <evel184@yandex.ru>
2022-11-23 14:13:09 +0700

    task #2.3

--- src/Main.java
+++ src/Main.java
@@ -39,5 +39,11 @@ public class Main {
         }
         System.out.println();
 
+        //task #2.3
+        for (int i = 1; i <= 512; i = i + i) {
+            System.out.print(i + " ") ;
+        }
+        System.out.println();
+
     }
 }
