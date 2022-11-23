1018ba70d7fb829ccb3cc403ead191b43494b28c
wlad184 <evel184@yandex.ru>
2022-11-23 14:07:04 +0700

    task #2.1

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 public class Main {
     public static void main(String[] args) {
 
-        //task #1
+        //task #1.1
         for (int i = 1; i <= 10; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
 
-        //task #2
+        //task #1.2
         for (int i = 10; i >= 1; i--) {
             System.out.print(i + " ");
         }
         System.out.println();
 
-        //task #3
+        //task #1.3
         for (int i = 1; i <= 17; i++) {
             if (i % 2 == 0) {
                 System.out.print(i + " ");
@@ -21,13 +21,16 @@ public class Main {
         }
         System.out.println();
 
-        //task #4
+        //task #1.4
         for (int i = 10; i >= -10; i--) {
             System.out.print(i + " ");
         }
         System.out.println();
 
-
+        //task #2.1
+        for (int i = 1904; i < 2096; i +=4) {
+            System.out.println(i + " год является високосным ");
+            }
 
     }
 }
